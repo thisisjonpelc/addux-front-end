fc185de33bf67038eff6a99767bba45cb2466f4d
Jon Pelc <thisisjonpelc@gmail.com>
2018-09-16 23:27:01 -0500

    Updated with feedback from Front End meeting

--- scripts/script.js
+++ scripts/script.js
@@ -34,28 +34,42 @@ for(var i = 0; i < labels.length; i++){
 
 var leftButton = document.querySelector("#scroll-left");
 var rightButton = document.querySelector("#scroll-right");
-var mainContent = document.querySelector(".main-content");
+//var mainContent = document.querySelector(".main-content");
+var mainContent = $('.main-content');
 
 leftButton.onclick = function(){
     console.log("SCROLL LEFT!");
-    mainContent.scrollLeft = mainContent.scrollLeft - 100;
+
+    mainContent.animate({scrollLeft: mainContent.scrollLeft() - 250}, 500);
 }
 
 rightButton.onclick = function(){
     console.log("SCROLL RIGHT!");
-    mainContent.scrollLeft = mainContent.scrollLeft + 100;
+    mainContent.animate({scrollLeft: mainContent.scrollLeft() + 250}, 500);
 }
 
 var appNavigation = document.querySelector(".app-nav");
 var listButton = appNavigation.children[3];
+var notesButton = appNavigation.children[2];
 var adduxList = document.querySelector(".addux-list");
+var notes = document.querySelector(".notes");
 
 listButton.onclick = function(){
     console.log("CLICKED LIST BUTTON");
     adduxList.classList.toggle("addux-list--active");
 }
 
+notesButton.onclick = function(){
+    console.log("CLICKED NOTES BUTTON");
+    notes.classList.toggle("notes--active");
+}
+
 adduxList.children[0].onclick = function(){
     console.log("CLICKED CLOSE BUTTON");
     adduxList.classList.remove("addux-list--active");
+}
+
+notes.children[0].onclick = function() {
+    console.log("CLICKED CLOSE BUTTON");
+    notes.classList.remove("notes--active");
 }
